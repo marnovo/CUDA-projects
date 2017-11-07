83e268b28c6cdf04fc14ecf72e3836c78e0a1351
Marcelo Novaes <mnovaes@gmail.com>
2017-11-07 06:39:34 +0100

    Add Array Addition C++ implementation
    Signed-off-by: Marcelo Novaes <mnovaes@gmail.com>

--- add-array/make-run-cpu.sh
+++ add-array/make-run-cpu.sh
@@ -1,2 +1,6 @@
-clang++ add-array-cpu.cpp -o ./bin/add-array-cpu
-./bin/add-array-cpu
+echo
+echo "********** Compile using clang++ compiler & time it **********"
+time clang++ add-array-cpu.cpp -o ./bin/add-array-cpu # -v
+echo
+echo "********** Run resulting binary & time it **********"
+time ./bin/add-array-cpu
